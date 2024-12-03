5b3c0155907f56cb0f7bf1c3ec8836c8a30ba234
MOHUND <msaadalharby@stu.kau.edu.sa>
2024-12-03 20:41:51 +0300

    Refactor routing to use BrowserRouter, update navigation links to use Link component, and remove NotFound route

--- src/App.js
+++ src/App.js
@@ -1,24 +1,22 @@
 import React from "react";
-import { HashRouter as Router, Route, Routes } from "react-router-dom";
+import { BrowserRouter as Router, Route, Routes, Link } from "react-router-dom";
 import Home from "./component/Home";
 import Quiz from "./component/Quiz";
 import About from "./component/About";
-import NotFound from "./component/NotFound";
 
 const App = () => {
   return (
     <Router>
       <div>
         <nav className="navbar">
-          <a href="/">Home</a>
-          <a href="#/about">About</a>
-          <a href="#/quiz">Quiz</a>
+          <Link to="/">Home</Link>
+          <Link to="/about">About</Link>
         </nav>
+
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/about" element={<About />} />
           <Route path="/quiz" element={<Quiz />} />
-          <Route path="*" element={<NotFound />} />
         </Routes>
       </div>
     </Router>
