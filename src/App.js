1b87e33d1e16d42eda47fe1013ecd964524b56fc
MOHUND <msaadalharby@stu.kau.edu.sa>
2024-12-03 20:39:16 +0300

    Update routing to redirect base path to /home and adjust links in navigation

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter as Router, Route, Routes, Link } from "react-router-dom";
+import { BrowserRouter as Router, Route, Routes, Link, Navigate } from "react-router-dom";
 import Home from "./component/Home";
 import Quiz from "./component/Quiz";
 import About from "./component/About";
@@ -9,12 +9,14 @@ const App = () => {
     <Router>
       <div>
         <nav className="navbar">
-          <Link to="/">Home</Link>
+          <Link to="/home">Home</Link>
           <Link to="/about">About</Link>
         </nav>
 
         <Routes>
-          <Route path="/" element={<Home />} />
+          {/* Redirect base path "/" to "/home" */}
+          <Route path="/" element={<Navigate to="/home" replace />} />
+          <Route path="/home" element={<Home />} />
           <Route path="/about" element={<About />} />
           <Route path="/quiz" element={<Quiz />} />
         </Routes>
